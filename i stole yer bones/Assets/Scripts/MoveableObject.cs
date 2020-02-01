5256e3687b03f34074d71bdd95c5fe4ba14ec523
Nick Rasmussen <nickras35@gmail.com>
2020-01-31 20:35:10 -0500

    super basic movable object
    just snaps to mouse position at this point

---
+++ i stole yer bones/Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour
+{
+    Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnMouseOver()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if(rb.bodyType != RigidbodyType2D.Dynamic)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
