e24e25b70f1dd9e900c8b3646e9adf1aeab3ded5
Nick Rasmussen <nickras35@gmail.com>
2020-01-31 20:57:41 -0500

    i can throw yer bones

--- i stole yer bones/Assets/Scripts/MoveableObject.cs
+++ i stole yer bones/Assets/Scripts/MoveableObject.cs
@@ -6,6 +6,9 @@ public class MoveableObject : MonoBehaviour
 {
     Rigidbody2D rb = null;
     bool isHeld = false; //is the mouse holding this objct?
+
+    Vector3 mouse;
+    Vector3 previousMousePosition;
     Vector3 mouseDisplacement; //how far from the center of the object the mouse is holding it
 
     // Start is called before the first frame update
@@ -17,15 +20,22 @@ public class MoveableObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (isHeld)
         {
             rb.bodyType = RigidbodyType2D.Kinematic;
-            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mouseDisplacement;
+            transform.position = mouse - mouseDisplacement;
+
         }
         else if(rb.bodyType != RigidbodyType2D.Dynamic)
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
+
+            //oh gee howdy do I love throwin' bones with some M A G I C N U M B E R S
+            rb.AddForce((mouse - previousMousePosition) * 25.0f, ForceMode2D.Impulse) ;
         }
+
+        previousMousePosition = mouse;
     }
 
 
@@ -39,7 +49,7 @@ public class MoveableObject : MonoBehaviour
                 mouseDisplacement = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             }
         }
-        else
+        else if(isHeld)
         {
             isHeld = false;
         }
